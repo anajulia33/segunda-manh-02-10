3566f27ca3b571eaf0026b8b4597ac73a0eba268
anajulia33 <146749830+anajulia33@users.noreply.github.com>
2023-10-02 12:04:51 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+num1 =  float(input("digite o primeiro número: "))
+num2 = float (input("digite o primeiro número: "))
+
+
+#realize operações
+soma= num1+ num2
+subtracao = num1-num2
+multiplicacao= num1 * num2
+divisao= num1 / num2
+#exige os resultados
+print (f"soma: {soma}")
+print(f"subtração : {subtracao}")
+print(f"multiplicação: {multiplicacao}")
+print (f"divisão: {divisao}")
